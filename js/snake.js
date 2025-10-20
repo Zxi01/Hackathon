a537426d361ec45fdbd9075ac0d285645654d195
Zxi01 <cameron.m.jennings@hotmail.com>
2025-10-20 10:35:24 +0100

    Add dpad controls for mobile/tablet users

--- js/snake.js
+++ js/snake.js
@@ -77,6 +77,21 @@ window.onload = function () {
 
     placeFood();
     document.addEventListener("keydown", changeDirection);
+
+    // D-pad button event listeners
+    document
+        .getElementById("dpad-up")
+        .addEventListener("click", () => setDirection("Up"));
+    document
+        .getElementById("dpad-down")
+        .addEventListener("click", () => setDirection("Down"));
+    document
+        .getElementById("dpad-left")
+        .addEventListener("click", () => setDirection("Left"));
+    document
+        .getElementById("dpad-right")
+        .addEventListener("click", () => setDirection("Right"));
+
     setInterval(update, 1000 / GAME_CONFIG.fps);
     updateScore();
 };
